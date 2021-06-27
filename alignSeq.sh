48f684c60adc6b794aff81e22582e40280a68d98
NavarroJ <59834493+NavarroJ@users.noreply.github.com>
2021-06-27 17:31:06 -0400

    Update alignSeq.sh

--- alignSeq.sh
+++ alignSeq.sh
@@ -5,4 +5,4 @@ bwa mem NC_045512.2.fasta ncov_global.fasta > ncov_global.sam
 samtools fixmate -O bam ncov_global.sam ncov_global.bam
 samtools sort -O bam -o ncov_global.sorted.bam ncov_global.bam
 samtools index ncov_global.sorted.bam
-samtools mpileup -f NC_045512.2.fasta -B -R -aa ncov_global.sorted.bam -o ncov_global.mpileup
+samtools mpileup -f NC_045512.2.fasta -B -R -aa ncov_global.sorted.bam --output-QNAME -o ncov_global.mpileup
